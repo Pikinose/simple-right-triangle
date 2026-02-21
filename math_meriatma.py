534d73069a0152cfb763defcb8e529490e39fa58
Pratyush Saha <pattaboissxp2010@gmail.com>
2026-02-21 15:27:52 +0530

    Add files via upload

---
+++ math_meriatma.py
@@ -0,0 +1,14 @@
+#we are going to make pascal's triangle
+import pandas
+
+rows = 10
+
+for i in range(rows):
+    print()
+    for fill in range(i+1):
+        khaali = " "*(rows-1)
+        
+        print("#", end=" ")
+
+
+print()
